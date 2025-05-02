d823fdc64e5f8ec19022eb10052bc686fff3ebdf
Carifio24 <carifio24@gmail.com>
2025-05-02 13:56:05 -0400

    Use NASA Grantee logo.

--- src/CreditLogos.vue
+++ src/CreditLogos.vue
@@ -11,7 +11,7 @@
         ><img alt="SciAct Logo" src="https://cosmicds.github.io/cds-website/logos/logo_sciact.png"
       /></a>
       <a href="https://nasa.gov/" target="_blank" rel="noopener noreferrer" class="pl-1"
-        ><img alt="SciAct Logo" src="https://cosmicds.github.io/cds-website/logos/NASA_Partner_color_300_no_outline.png"
+        ><img alt="SciAct Logo" src="https://cosmicds.github.io/cds-website/logos/NASA_Grantee_color_no_outline.png"
       /></a>
     </div>
   </div>
@@ -19,10 +19,9 @@
 
 
 <script lang="ts">
-import { defineComponent, } from "vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-
 
   props: {
     visible: {
